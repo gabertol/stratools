b145870e6ad65b86a2484e8d76a47b3b9de39235
Gabriel Bertolini <gabertol@gmail.com>
2024-01-22 19:03:22 -0300

    cex function added to logplot

--- R/log_plot.R
+++ R/log_plot.R
@@ -46,7 +46,8 @@ log_plot<-function(data,
                    grainsize=TRUE,
                    y_scale=TRUE,
                    x_scale=TRUE,
-                   gs_less_size=5){
+                   gs_less_size=5,
+                   cex=0.5){
 
   # Variables
   sample<-data %>% dplyr::select(sample=sample_name)
@@ -157,7 +158,7 @@ log_plot<-function(data,
   } else {}
 
   multigons(basic.log$i, basic.log$xy, basic.log$dt, col = DF_loc$col)
-  bedtext(labels = DF_loc$facies1, l = DF_loc$l, r = DF_loc$r, x = -0.5, ymin = NA, arg = list(cex = 0.5))
+  bedtext(labels = DF_loc$facies1, l = DF_loc$l, r = DF_loc$r, x = -0.5, ymin = NA, arg = list(cex = cex))
 
   # Calcular a posição central com base na escala x
   x_center <- (par()$usr[1] + par()$usr[2]) / 2
